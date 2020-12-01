126bb4f39c6b6a3b30b0045599a736d2c3a73f0a
Tanel Otsus <tanel.otsus@khk.ee>
2020-12-01 09:14:49 +0200

    Ülesanne: Apache paigaldus ja kontroll skripti abil

--- apache_paigaldus.sh
+++ apache_paigaldus.sh
@@ -2,13 +2,14 @@
 ## Apache2 paigaldamine masinasse scripti abil.
 ##Tuleb tuvastada kas masinas  programm on olemas.
 
-programm=$(dpkg-query -W -f='${Status}' apache2 | grep -c 'ok installed')
+programm=$(dpkg-query -W -f='${Status}' apache2 2>/dev/null | grep -c 'ok installed')
 if  [ $programm -eq 0 ];then
-	echo "Programmi paigaldus"
-	apt update | apt install apache2
+	echo "Programmi paigaldus" #kui ei ole toimub programmi paigaldus
+	apt install apache2
 	echo "Programm on paigaldatud"
+
 elif [ $programm -eq 1 ]; then
-	echo "Programm on varasemalt paigaldatud"
-	service apache2 start
-	service apache status
+	echo "Programm on varasemalt paigaldatud" #kui programm on olemas tooimub programmi käivius ja kontroll
+	systemctl start apache2.service #service apache2 start
+	systemctl status apache2 #service apache2 status
 fi 
